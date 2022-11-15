35e419f589f30be4f662145f34ecf6dd8f8f6556
Jack Potier <jackpotierschool@gmail.com>
2022-11-16 12:08:02 +1300

    encrypted decimal to hex

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ function hexToDecimal(unicodeHexArray) {
 }
 hexToDecimal(unicodeHexArray)
 
+
 function decimalShift(unicodeDecimalArray) {
     updatedDecimalArray = [];
     counter2 = 0;
@@ -38,8 +39,31 @@ function decimalShift(unicodeDecimalArray) {
         if (unicodeDecimalArray[counter2] >= 97 && unicodeDecimalArray[counter2] < 112){
             updatedDecimalArray[counter2] = unicodeDecimalArray[counter2]+shift;
         }
+        else {
+            updatedDecimalArray[counter2] = unicodeDecimalArray[counter2]
+        }
         counter2+=1
     }
     console.log("Encrypted Decimal Array:",updatedDecimalArray);
 }
 decimalShift(unicodeDecimalArray)
+
+function newDecimalToHex(updatedDecimalArray){
+    updatedHexArray = [];
+    counter3 = 0;
+        while (counter3 < updatedDecimalArray.length){
+            if ((updatedDecimalArray[counter3] >=65 && updatedDecimalArray[counter3] <=(90+shift)) || (updatedDecimalArray[counter3] >=97 && updatedDecimalArray[counter3] <=(113+shift))){
+                updatedHexArray[counter3] = updatedDecimalArray[counter3].toString(16);
+                while (updatedHexArray[counter3].length < 4) {
+                    updatedHexArray[counter3] = '0' + updatedHexArray[counter3];
+                }
+                updatedHexArray[counter3] = "\\u" + updatedHexArray[counter3];
+            }
+            else {
+                updatedHexArray[counter3] = updatedDecimalArray[counter3]
+            }
+            counter3+=1
+            }
+    console.log("Encrypted Hex Array:",updatedHexArray);
+}
+newDecimalToHex(updatedDecimalArray);
